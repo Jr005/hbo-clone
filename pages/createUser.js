a58055549bf39980763015c3d710103375494455
Ruben esquivel <rubenesquivel@Rubens-iMac.local>
2021-09-04 20:11:44 -0500

    I updated the css for create user

--- pages/createUser.js
+++ pages/createUser.js
@@ -10,31 +10,61 @@ export default function CreateUser() {
 					<span className='create-user__title'>Who is Watching!</span>
 				</div>
 				<div className='create-user__form'>
-					<div className='create-user__user-box'>
-						<img
-							src='https://images.unsplash.com/photo-1549351512-c5e12b11e283?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-							alt=''
-							className='create-user__user-img'
-						/>
-						<div className='create-user__input-group'>
-							<label>Name</label>
-							<input type='text' className='create-user__inputText' />
-							<div className='create-user__colors'>
-								<div
-									className='create-user__color create-user__color--active'
-									style={{
-										background: "rgb(2,27,64)",
-										background:
-											"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
-									}}
-								></div>
-							</div>
+					<img
+						src='https://images.unsplash.com/photo-1549351512-c5e12b11e283?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+						alt=''
+						className='create-user__user-img'
+					/>
+					<div className='create-user__input-group'>
+						<label>Name</label>
+						<input type='text' className='create-user__inputText' />
+						<div className='create-user__colors'>
+							<div
+								className='create-user__color create-user__color--active'
+								style={{
+									background: "rgb(2,27,64)",
+									background:
+										"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
+								}}
+							/>
+							<div
+								className='create-user__color create-user__color--active'
+								style={{
+									background: "rgb(2,27,64)",
+									background:
+										"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
+								}}
+							/>
+							<div
+								className='create-user__color create-user__color--active'
+								style={{
+									background: "rgb(2,27,64)",
+									background:
+										"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
+								}}
+							/>
+							<div
+								className='create-user__color create-user__color--active'
+								style={{
+									background: "rgb(2,27,64)",
+									background:
+										"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
+								}}
+							/>
+							<div
+								className='create-user__color create-user__color--active'
+								style={{
+									background: "rgb(2,27,64)",
+									background:
+										"linear-gradient(135deg, rgba(2,27,64,1) 0%, rgba(119,30,135,1) 100%)",
+								}}
+							/>
 						</div>
 					</div>
 				</div>
 				<div className='create-user__buttons'>
-					<button className='create-user__adult'>Add Adult</button>
-					<button className='create-user__kid'>Add Kid</button>
+					<button className='create-user__cancel'>Cancel</button>
+					<button className='create-user__save'>Save</button>
 				</div>
 			</div>
 		</div>
